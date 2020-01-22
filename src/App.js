1c0b37e2343614e4fa8a24a1ab8de2a9e6a24111
fbieg <fbieg@intellisys.com.do>
2020-01-22 11:30:29 -0400

    feat: Implementing react router dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Router>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/new-question">New Question</Link>
+            </li>
+            <li>
+              <Link to="/leader-board">Leader Board</Link>
+            </li>
+          </ul>
+        </nav>
       </header>
-    </div>
+
+      <Switch>
+        <Route path="/leader-board">
+          Rank by score (answered + created questions)
+        </Route>
+        <Route path="/new-question">
+          Add a new question :D
+        </Route>
+        <Route path="/question/:id">
+          Add answer to a question
+          if the question is answered, show percentage table
+        </Route>
+        <Route path="/">
+          If the user is logged in, show the home (answered and unanswered questions)
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
